4f73b3b52175d43116ab85ca46a6c180b034c42d
Mohamed Mohamed <mohd@mohd.ca>
2024-07-29 10:59:44 +0100

    create new lambda

---
+++ amplify/backend/function/RukuAPIQueryHandler/src/index.ts
@@ -0,0 +1,13 @@
+export const handler = async (event: Event): Promise<any> => {
+    console.info(`EVENT: ${JSON.stringify(event)}`);
+
+    return {
+        statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    //  headers: {
+    //      "Access-Control-Allow-Origin": "*",
+    //      "Access-Control-Allow-Headers": "*"
+    //  },
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+};
